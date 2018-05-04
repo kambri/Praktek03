b09fd899b8e5fdb2fde814ec654d9b4f3c2285c0
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 21:04:15 +0700

    set attribute private

--- src/praktek03/Bola.java
+++ src/praktek03/Bola.java
@@ -1,9 +1,9 @@
 package praktek03;
 
 public class Bola {
-    String merk;
-    String warna;
-    double harga;
+    private String merk;
+    private String warna;
+    private double harga;
     
     void cetakInfo(){
         System.out.println("Merk Bola \t: "+merk+"\n"+
