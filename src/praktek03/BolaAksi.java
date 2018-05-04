e0629c8b6a164211e464b2bbccfc2aede594513c
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 21:15:08 +0700

    menggunakan method get

--- src/praktek03/BolaAksi.java
+++ src/praktek03/BolaAksi.java
@@ -7,6 +7,13 @@ public class BolaAksi {
         a.setHarga(250000);
         
         a.cetakInfo();
+        
+        System.out.print(" Merk bola ini \t : ");
+        System.out.println(a.getMerk());
+        System.out.print(" Berwarna \t : ");
+        System.out.println(a.getWarna());
+        System.out.print(" Dengan Harga \t : ");
+        System.out.println(a.getHarga());
     }
     
 }
