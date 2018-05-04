121092d4f010f81076f57c272282495061d03bbe
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 20:48:38 +0700

    Buat method cetakinfo()

--- src/praktek03/Bola.java
+++ src/praktek03/Bola.java
@@ -4,4 +4,10 @@ public class Bola {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk Bola \t: "+merk+"\n"+
+                "Berwarna \t: "+warna+"\n"+
+                "Seharga  \t: "+harga);
+    }
 }
