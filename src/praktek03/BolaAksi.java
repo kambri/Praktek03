b2c3d53bf11b7adfc3a715ec0ee0cd79d60455ec
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 21:02:33 +0700

    instance buat attribute dari panggil method

--- src/praktek03/BolaAksi.java
+++ src/praktek03/BolaAksi.java
@@ -1,7 +1,12 @@
 package praktek03;
 public class BolaAksi {
     public static void main (String[] args){
+        Bola a = new Bola();
+        a.merk = "Adidas";
+        a.warna = "Hitam";
+        a.harga = 230000;
         
+        a.cetakInfo();
     }
     
 }
