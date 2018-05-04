417d5b64f4bf2ebfd06b9f69b139b560e574febc
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 21:23:23 +0700

    attribute dan method

--- src/praktek03/BolaBasket.java
+++ src/praktek03/BolaBasket.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class BolaBasket extends Bola {
+    private String jenis;
+    
+    void throttle(){
+        System.out.println("DRIBEL..PASSING..SHOOT !!");
+    }
     
 }
