a577014148e45847fffdcb01913b97d71d8b16bb
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 21:11:10 +0700

    menggunakan method set

--- src/praktek03/BolaAksi.java
+++ src/praktek03/BolaAksi.java
@@ -2,9 +2,9 @@ package praktek03;
 public class BolaAksi {
     public static void main (String[] args){
         Bola a = new Bola();
-        a.merk = "Adidas";
-        a.warna = "Hitam";
-        a.harga = 230000;
+        a.setMerk("Nike");
+        a.setWarna("Abu-Abu");
+        a.setHarga(250000);
         
         a.cetakInfo();
     }
