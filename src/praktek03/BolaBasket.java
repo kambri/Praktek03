7592fe020115db9b13a065438b16b9ba12b90099
kambri <37506225+kambri@users.noreply.github.com>
2018-05-04 21:24:37 +0700

    Getter And Setter

--- src/praktek03/BolaBasket.java
+++ src/praktek03/BolaBasket.java
@@ -6,5 +6,15 @@ public class BolaBasket extends Bola {
     void throttle(){
         System.out.println("DRIBEL..PASSING..SHOOT !!");
     }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+    
+    
     
 }
